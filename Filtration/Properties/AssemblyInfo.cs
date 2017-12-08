1ecec4e9dc345b75eee41023d64af8ff325a7405
Ben Wallis <atomyc+github@gmail.com>
2017-12-08 17:58:53 +0000

    Bumped version to 0.17

--- Filtration/Properties/AssemblyInfo.cs
+++ Filtration/Properties/AssemblyInfo.cs
@@ -50,7 +50,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16")]
+[assembly: AssemblyVersion("0.17")]
 
 [assembly: InternalsVisibleTo("Filtration.Tests")]
 [assembly: InternalsVisibleTo("Filtration.ItemFilterPreview.Tests")]
