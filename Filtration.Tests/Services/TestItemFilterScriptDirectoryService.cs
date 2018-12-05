5fc260dbb27286bfcf704aef10d329cb4292e322
Ben Wallis <ben.wallis@datapath.co.uk>
2018-12-05 12:42:01 +0000

    Bumped version to 1.1.0-beta5

--- Filtration.Tests/Services/TestItemFilterScriptDirectoryService.cs
+++ Filtration.Tests/Services/TestItemFilterScriptDirectoryService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Filtration.Common.Services;
+﻿using Filtration.Common.Services;
 using Filtration.Services;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using Moq;
