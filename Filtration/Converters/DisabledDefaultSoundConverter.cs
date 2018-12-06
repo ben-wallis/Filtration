72b874e7d5a40be0168aff3428b7a10e06daff96
GlenCFL <glencfl@gmail.com>
2018-12-06 09:16:30 -0500

    Add a DisableDropSound indicator.

---
+++ Filtration/Converters/DisabledDefaultSoundConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace Filtration.Converters
+{
+    public class DisabledDefaultSoundConverter : IValueConverter
+    {
+        private static readonly BitmapImage _soundIcon;
+        private static readonly BitmapImage _soundDDSIcon;
+
+        static DisabledDefaultSoundConverter()
+        {
+            var soundUri = new Uri("pack://application:,,,/Filtration;component/Resources/Icons/sound.png", UriKind.Absolute);
+            var soundDDSUri = new Uri("pack://application:,,,/Filtration;component/Resources/Icons/sound_dds.png", UriKind.Absolute);
+            _soundIcon = new BitmapImage(soundUri);
+            _soundDDSIcon = new BitmapImage(soundDDSUri);
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (bool)value ? _soundDDSIcon : _soundIcon;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ReferenceEquals(value, _soundDDSIcon);
+        }
+    }
+}
