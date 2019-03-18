717a0a83a954a78fe205af0019016cf22703427a
Ben Wallis <atomyc+github@gmail.com>
2019-03-18 17:48:16 +0000

    * Bumped version to 1.2.0 * Added enchantments to Enchantments.txt * Updated nuspec

--- Filtration/Properties/AssemblyInfo.cs
+++ Filtration/Properties/AssemblyInfo.cs
@@ -10,8 +10,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 
 [assembly: InternalsVisibleTo("Filtration.Tests")]
 [assembly: InternalsVisibleTo("Filtration.ItemFilterPreview.Tests")]
